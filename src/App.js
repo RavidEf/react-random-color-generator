e486efb7481ac5acb6c2cf072276c867f6dba0bf
Ravid Efroni <ravidefroni@gmail.com>
2024-09-16 19:19:11 +0200

    Fix the useState function

--- src/App.js
+++ src/App.js
@@ -1,106 +1,84 @@
 import './App.css';
 import randomColor from 'randomcolor';
-import { useState } from 'react';
+import { React, useRef, useState } from 'react';
 
 export default function App() {
-  const changeColor = randomColor();
-  const [color, setColor] = useState('#f2af85');
-  const click = (color) => {
-    setColor(color);
-    // eslint-disable-next-line no-restricted-syntax
-    document.getElementById('buttonDiv').style.backgroundColor = changeColor;
-  };
+  const initialColor = randomColor();
+  const [color, setColor] = useState(initialColor);
 
   return (
-    <div
-      style={{
-        fontFamily: 'monospace',
-        justifyContent: 'center',
-        border: '10px solid #078080',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        margin: '10px',
-        padding: '20px',
-      }}
-    >
-      <h1
-        style={{
-          margin: '20px',
-          padding: '20px',
-          alignContent: 'center',
-        }}
-      >
-        Random color generator
-      </h1>
-      <p
-        style={{
-          fontSize: '15px',
-          alignContent: 'center',
-          margin: '5px',
-          padding: '5px',
-        }}
-      >
-        Click on the button below to generate a color.
-      </p>
-
-      <button
-        onClick={() => click(changeColor)}
-        id="colorinput"
-        name="colorinput"
-        type="button"
-        className="inputField"
-        style={{
-          margin: '20px',
-          padding: '20px',
-          borderRadius: '20px',
-          backgroundColor: '#8cd4fa',
-        }}
-      >
-        Generate
-      </button>
-
+    <body>
       <div
-        id="buttonDiv"
         style={{
-          borderRadius: '20px',
-          backgroundColor: '#f2af85',
-          animation: 'ease-in',
-          transition: '1s',
+          fontFamily: 'monospace',
+          justifyContent: 'center',
+          border: '10px solid #078080',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          margin: '10px',
+          padding: '20px',
         }}
       >
-        <p
+        <h1
           style={{
-            fontSize: '25px',
-            alignContent: 'center',
             margin: '20px',
             padding: '20px',
+            alignContent: 'center',
           }}
         >
-          Generated Color: {color}
+          Random color generator
+        </h1>
+        <p
+          style={{
+            fontSize: '15px',
+            alignContent: 'center',
+            margin: '5px',
+            padding: '5px',
+          }}
+        >
+          Click on the button below to generate a color.
         </p>
+
+        <button
+          onClick={() => {
+            const newColor = randomColor();
+            setColor(newColor);
+          }}
+          id="colorinput"
+          name="colorinput"
+          className="inputField"
+          style={{
+            margin: '20px',
+            padding: '20px',
+            borderRadius: '20px',
+            backgroundColor: '#8cd4fa',
+          }}
+        >
+          Generate
+        </button>
+
+        <div
+          id="buttonDiv"
+          style={{
+            borderRadius: '20px',
+            backgroundColor: `${color}`,
+            animation: 'ease-in',
+            transition: '0.75s',
+          }}
+        >
+          <p
+            style={{
+              fontSize: '25px',
+              alignContent: 'center',
+              margin: '20px',
+              padding: '20px',
+            }}
+          >
+            Generated Color: {color}
+          </p>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
-
-/*
-      <label for="colorinput" />
-      <input
-        value={color}
-        onClick={
-          (event) => setColor(event.currentTarget.value)
-           const foundColor = get(event.currentTarget.value);
-          {color}
-
-        id="colorinput"
-        name="colorinput"
-        type="button"
-        className="inputField"
-        style={{
-          margin: '20px',
-          padding: '20px',
-          borderRadius: '20px',
-          backgroundColor: '#5e1493',
-        }
-      />  */
