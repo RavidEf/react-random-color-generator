e6d485fc7b2082282f323decf22f161fda4e0fc8
Ravid Efroni <ravidefroni@gmail.com>
2024-09-16 19:33:03 +0200

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import randomColor from 'randomcolor';
 import { useState } from 'react';
 
@@ -7,78 +6,76 @@ export default function App() {
   const [color, setColor] = useState(initialColor);
 
   return (
-    <body>
-      <div
+    <div
+      style={{
+        fontFamily: 'monospace',
+        justifyContent: 'center',
+        border: '10px solid #078080',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '10px',
+        padding: '20px',
+      }}
+    >
+      <h1
         style={{
-          fontFamily: 'monospace',
-          justifyContent: 'center',
-          border: '10px solid #078080',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          margin: '10px',
+          margin: '20px',
           padding: '20px',
+          alignContent: 'center',
         }}
       >
-        <h1
-          style={{
-            margin: '20px',
-            padding: '20px',
-            alignContent: 'center',
-          }}
-        >
-          Random color generator
-        </h1>
+        Random color generator
+      </h1>
+      <p
+        style={{
+          fontSize: '15px',
+          alignContent: 'center',
+          margin: '5px',
+          padding: '5px',
+        }}
+      >
+        Click on the button below to generate a color.
+      </p>
+
+      <button
+        onClick={() => {
+          const newColor = randomColor();
+          setColor(newColor);
+        }}
+        id="colorinput"
+        name="colorinput"
+        className="inputField"
+        style={{
+          margin: '20px',
+          padding: '20px',
+          borderRadius: '20px',
+          backgroundColor: '#8cd4fa',
+        }}
+      >
+        Generate
+      </button>
+
+      <div
+        id="buttonDiv"
+        style={{
+          borderRadius: '20px',
+          backgroundColor: `${color}`,
+          animation: 'ease-in',
+          transition: '0.75s',
+        }}
+      >
         <p
           style={{
-            fontSize: '15px',
+            fontSize: '25px',
             alignContent: 'center',
-            margin: '5px',
-            padding: '5px',
-          }}
-        >
-          Click on the button below to generate a color.
-        </p>
-
-        <button
-          onClick={() => {
-            const newColor = randomColor();
-            setColor(newColor);
-          }}
-          id="colorinput"
-          name="colorinput"
-          className="inputField"
-          style={{
             margin: '20px',
             padding: '20px',
-            borderRadius: '20px',
-            backgroundColor: '#8cd4fa',
           }}
         >
-          Generate
-        </button>
-
-        <div
-          id="buttonDiv"
-          style={{
-            borderRadius: '20px',
-            backgroundColor: `${color}`,
-            animation: 'ease-in',
-            transition: '0.75s',
-          }}
-        >
-          <p
-            style={{
-              fontSize: '25px',
-              alignContent: 'center',
-              margin: '20px',
-              padding: '20px',
-            }}
-          >
-            Generated Color: {color}
-          </p>
-        </div>
+          Generated Color: {color}
+        </p>
       </div>
-    </body>
+    </div>
   );
 }
